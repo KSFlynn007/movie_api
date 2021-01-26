90fb2e0bd7252fa925432b84265dd135dbc4d6f5
KSFlynn007 <kelseyflynnn@gmail.com>
2021-01-26 15:06:44 -0700

    Update movies-router.js
    removed all JWT authoirzation logic from /movies endpoint. Needs to be working version on Heroku too

--- movies/movies-router.js
+++ movies/movies-router.js
@@ -5,7 +5,7 @@ const Movies = Models.Movie;
 const passport = require('passport');
 
 MovieRouter //all endpoints have /movies implied as they are within the movies-router
-.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+.get('/', (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
@@ -16,7 +16,7 @@ MovieRouter //all endpoints have /movies implied as they are within the movies-r
         });
 })
 //get data about single movie by title
-.get('/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
+.get('/:Title', (req, res) => {
     Movies.findOne({ Title: req.params.Title })
         .then((movie) => {
             res.json(movie);
@@ -27,7 +27,7 @@ MovieRouter //all endpoints have /movies implied as they are within the movies-r
         });
 })
 //get data about genre by title "thriller"
-.get('/genres/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
+.get('/genres/:Name', (req, res) => {
 // This is how it is searched by - by name of GENRE, not name of MOVIE
     Movies.findOne({ 'Genre.Name': req.params.Name })
 // This is what is returned in JSON format, I only need Genre Name + Genre Description, not entire movie details
@@ -40,7 +40,7 @@ MovieRouter //all endpoints have /movies implied as they are within the movies-r
     });
 })
 //get data about a director, same format as above by searching in nested content?
-.get('/directors/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
+.get('/directors/:Name', (req, res) => {
     Movies.findOne({ 'Director.Name': req.params.Name })
     .then((movie) => {
         res.status(201).json(movie.Director.Name + ': ' + movie.Director.Bio);
